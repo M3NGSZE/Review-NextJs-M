1c3c45ffaf6ed3332320a4fa3aaf16f880b7a1a9
M3NGSZE <mengse.thun13@gmail.com>
2025-08-05 00:10:46 +0700

    component folder

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from './components/Button'
 
 export default function page() {
     return (
         <div>
             <h1 className='text-5xl'>This contact page</h1>
+            <Button/>
         </div>
     )
 }
