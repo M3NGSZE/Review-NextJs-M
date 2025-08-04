e0b603af9ccd2a91f005d8aa7ff03084e4dd223e
M3NGSZE <mengse.thun13@gmail.com>
2025-08-04 23:51:30 +0700

    routing in next

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <h1 className='text-3xl text-red-700'>This is about page</h1>
+        </div>
+    )
+}
